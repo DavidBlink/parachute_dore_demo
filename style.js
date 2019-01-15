13d5102da0baafa4ed76f2ba84b6b5cda5472206
DavidBlink <39801596+DavidBlink@users.noreply.github.com>
2019-01-15 11:30:13 +0100

    Update style.js

--- style.js
+++ style.js
@@ -39,6 +39,7 @@
 				{ name: 'CajaInicio', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-1' } },
 				{ name: 'CajaLeson', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-2' } },
 				{ name: 'CajaEsquinaVerde', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-3' } },
+				{ name: 'CajaTachesAzul', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-10' } },
 				{ name: 'CajaAmarilla', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-4' } },
 				{ name: 'CajaBordeAzul', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-5' } },
 				{ name: 'CajaFondoAzul', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box-6' } },
