ab8f18c3ae971db978d763e3f138a7c6917af7cd
DavidBlink <39801596+DavidBlink@users.noreply.github.com>
2019-02-06 11:52:43 +0100

    Update style.js

--- style.js
+++ style.js
@@ -53,6 +53,7 @@
 				{ name: 'Lista Negrita', element: 'ol', attributes: { 'class': 'bck-ol-7' } },
 				{ name: 'Lista OrdAmarilla', element: 'ol', attributes: { 'class': 'bck-ol-4' } },
 				{ name: 'Lista CírculoNegro', element: 'ol', attributes: { 'class': 'bck-ol-6' } },
+				{ name: 'Lista NumeroAzul', element: 'ol', attributes: { 'class': 'bck-ol-8' } },
 				{ name: 'Lista LetraMinusAzul', element: 'ol', attributes: { 'class': 'bck-ol-2' } },
 				{ name: 'Lista LetraMinusMorada', element: 'ol', attributes: { 'class': 'bck-ol-5' } },
 				{ name: 'Lista RectaAmarilla', element: 'ol', attributes: { 'class': 'bck-ol-3' } },
